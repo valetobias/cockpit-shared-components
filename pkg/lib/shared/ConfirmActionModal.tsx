34170d7ffccc36af4c63dfbc63e3e617f7d49a57
Tobias Vale <tobias.vale@protonmail.com>
2025-03-26 11:52:55 +0100

    Added ConfirmActionModal Component

---
+++ pkg/lib/shared/ConfirmActionModal.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, Modal, ModalVariant } from '@patternfly/react-core'
+
+interface ConfirmActionProps {
+  action: () => void;
+  openingValidation: () => boolean;
+  variant?: "link" | "primary" | "secondary" | "tertiary" | "danger" | "warning" | "plain" | "control" | undefined;
+  message: string;
+  buttonText: string;
+}
+
+
+export const ConfirmActionModal: React.FunctionComponent<ConfirmActionProps> = ({ action, variant='primary', message, buttonText, openingValidation }) => {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  const handleModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+
+
+  return (
+    <React.Fragment>
+      <Button variant={variant} onClick={handleModalToggle}>
+        {buttonText}
+      </Button>
+      <Modal
+        variant={ModalVariant.small}
+        title="Confirm action"
+        isOpen={isModalOpen}
+        onClose={handleModalToggle}
+        actions={[
+          <Button key="confirm" variant={variant} onClick={() => {if (openingValidation()) {handleModalToggle(); action();}}}>
+            Confirm
+          </Button>,
+          <Button key="cancel" variant="link" onClick={handleModalToggle}>
+            Cancel
+          </Button>
+        ]}
+      >
+        {message}
+      </Modal>
+    </React.Fragment>
+  );
+}
